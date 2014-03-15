4b365429eef6a924400ace0a9e9d41f62950dea6
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-03-15 10:07:51 -0700

    comment checkin

---
+++ project.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+'''
+f(x,y,z,u,v,w,θ) =
+
+⌊                                                       ⌋
+  u(ux + vy + wz )(1 - cosθ) + x cosθ + (- wy + vz) sin θ
+|                                                       |
+||  v(ux + vy + wz )(1 - cosθ) + ycos θ + (wx - uz) sin θ ||
+⌈                                                       ⌉
+  w(ux +  vy + wz )(1 - cos θ) + z cosθ + (- vx + uy) sin θ
+
+'''

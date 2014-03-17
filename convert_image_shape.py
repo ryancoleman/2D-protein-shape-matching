23a26659317c8d8be5ae35fe0d57d7633d619914
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-03-17 14:46:17 -0700

    more shapes

--- convert_image_shape.py
+++ convert_image_shape.py
@@ -12,8 +12,9 @@ size = 256  # should be enough for anybody
 shapeDir = 'shapes'
 
 for oneFile in glob.iglob(os.path.join(shapeDir, '*.txt')):  # every file
+  oneName = string.split(os.path.split(oneFile)[-1], '.')[0]
   matrix = project.make2dMapFromShape(oneFile, size)
-  pngfile = open('tmp.png', 'wb')
+  pngfile = open(os.path.join(shapeDir, oneName + '.png'), 'wb')
   pngwriter = png.Writer(size, size, greyscale=True, bitdepth=1)
   pngwriter.write(pngfile, matrix)
   pngfile.close()
